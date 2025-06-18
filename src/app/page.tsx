16189fd32024447c2e87e3e40fc7468f5f847069
joaovx1315 <joaovitorvx1315@gmail.com>
2025-06-18 20:09:07 -0300

    event/ subimit/

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,28 @@
 "use client";
-//separando os alerts 
-import { CustomButton } from "./components/CustomButton";
-import "./globals.css";
+//input basico
+// //sempre lembrar dos import
+// // (event: FormEvent<HTMLFormElement>) expecificando de onde vem
 
+import "./globals.css";
+import { FormEvent } from "react";
 
 const Page = () => {
-
-  const handleButtton1 = ()=> alert('aqui 1')
-    const handleButtton2 = ()=> alert('aqui 2')
-      const handleButtton3 = ()=> alert('aqui 3')
-
-
+  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    alert("opa, aqui 1");
+  };
 
   return (
-    <div className="w-screen h-screen flex justify-center items-center gap-4">
-      <CustomButton label="aqui1"  onClick={handleButtton1}/>
-            <CustomButton label="aqui2" onClick={handleButtton2}/>
-                  <CustomButton label="aqui3" onClick={handleButtton3}/>
-
+    <div className="w-screen h-screen flex flex-col justify-center items-center gap-6">
+      <h1 className="text-5xl">Form login</h1>
+      <form onSubmit={handleFormSubmit} className="flex flex-col gap-4">
+        <input type="text" className="p-2 border border-gray-300 rounded" />
+        <input
+          type="submit"
+          value="Enviar"
+          className="bg-blue-600 text-white py-2 rounded hover:bg-blue-700 transition"
+        />
+      </form>
     </div>
   );
 };
