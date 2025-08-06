0af6338b4c7708cb37d16f209f7b3036fd911cd1
joaovx1315 <joaovitorvx1315@gmail.com>
2025-08-06 15:56:39 -0300

    video player/ com a/url/Mp4

--- form-zod/src/app/page.tsx
+++ form-zod/src/app/page.tsx
@@ -1,31 +1,27 @@
 "use client";
+import {useState} from "react";
+import {VideoPlayer} from './components/VideoPlayer';
 
-import { useState, useEffect } from "react";
+const Page= () => {
+const [playing, setPlaying] = useState(false);
 
-const Page = () => {
-  const [firstName, setFirstName] = useState("João");
-  const [lastName, setLastName] = useState("Silva");
- 
-  const fullName = firstName + " " + lastName;
 
   return (
-    <div className="p-6">
-      <h1 className="text-xl font-semibold mb-2">My Name</h1>
-      <p className="text-lg mb-4">{fullName}</p>
-
-      <hr/>
-
-      <div className="flex gap-4">
-        <button
-          className="border border-red-500 px-4 py-2 rounded hover:bg-red-100"
-          onClick={() => setFirstName("Vitor")}>
-          Set to Vitor
-        </button>
-
-        <button className="border border-red-500 px-4 py-2 rounded hover:bg-red-100"onClick={() => setFirstName("Pedro")}>
-          Set to Pedro
-        </button>
+    <div className="">
+      <div className="border border-white p-3 mb-5 ">
+        <p className="text-2xl text-blue-500 mb-3 "> {playing ? 'RODANDO' : 'PAUSADO'}</p>
+        <button className=" bg-blue-500 rounded-md p-3 text-black" onClick={()=> setPlaying(!playing)}> play/pause </button>
       </div>
+     
+      <div className="flex justify-center items-center min-h-screen">
+      <VideoPlayer 
+         src="https://media.istockphoto.com/id/2121488068/pt/v%C3%ADdeo/close-up-illustration-of-a-shoujo-manga-protagonist-with-white-eyes-in-shock.mp4?s=mp4-640x640-is&k=20&c=_OvdrBV-ieXYe7-Nbt1CCFYPk0UrRVlrJMKC7p6CyWk="
+         isPlaying={playing}
+       />
+      </div>
+   
+
+    
     </div>
   );
 };
