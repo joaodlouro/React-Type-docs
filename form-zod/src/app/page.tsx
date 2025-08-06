aad87e58982cd170eb9293ec310aa632f3416db7
joaovx1315 <joaovitorvx1315@gmail.com>
2025-08-06 12:26:00 -0300

    name and age

--- form-zod/src/app/page.tsx
+++ form-zod/src/app/page.tsx
@@ -1,16 +1,27 @@
 "use client";
-import { useEffect } from "react";
 
-const Page = () => {
-  
+import { useState, useEffect } from 'react';
+
+ const Page = () => {
+  const [name, setName] = useState('joaozonho123');
+  const [age, setAge] = useState(50);
+
+
   useEffect(() => {
-   
-    console.log("Page component mounted");
-  }, []);
+    console.log ('rodando :p');
+
+  }, [age]);
 
   return (
-    <div>
-       aqui teste
+    <div className="">
+
+      <p>nome aqui e {name} e eu tenho {age} receba</p>
+       <hr/>
+       <button className= "btn p-3 btn-primary border-color bg-red-500" onClick={() => setName('joaozonho123')}> mudar  </button>
+       <button className= "btn p-3 btn-primary border-color bg-red-500" onClick={() => setName('Pedro')}> mudar  </button>
+       <button className= "btn p-3 btn-primary  bg-red-500" onClick={() => setAge(50)}> mudar idade </button>
+        <button className= "btn p-3 btn-primary  bg-red-500" onClick={() => setAge(25)}> mudar idade </button>
+
     </div>
   );
 }
