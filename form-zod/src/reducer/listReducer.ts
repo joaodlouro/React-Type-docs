b2317c7d7f993e4f3da9b7033f6c833ad86987e2
joaovx1315 <joaovitorvx1315@gmail.com>
2025-08-07 18:41:39 -0300

    listReducer completo

--- form-zod/src/reducer/listReducer.ts
+++ form-zod/src/reducer/listReducer.ts
@@ -33,6 +33,32 @@ type removeAction = {
 type ListActions = AddAction | EditTextAction | ToggleDoneAction | removeAction ;
 
 export const listReducer = (list: Item[], action: ListActions ) =>{
+  switch (action.type) {
+    case 'add':
+      return [...list,{
+        id: list.length,
+        text: action.payload.text,
+        done: false
+      }]
+      case 'editText':
+        return list.map(t =>{
+          if (t.id === action.payload.id){
+            t.text = action.payload.newText
 
-  return list;
+          }
+          return t 
+        });
+
+        case 'toggleDone':
+          return list.map(t=>{
+            if (t.id === action.payload.id) t.done = !t.done;
+            return t
+          });
+
+          case 'remove':
+            return list.filter(t => t.id !== action.payload.id);
+            default:
+              return list;
+  }
+
 }
