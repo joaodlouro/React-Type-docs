3b1b15decf295fff22d46cff9cc070f8292a0e11
joaodlouro <joaovitorvx1315@gmail.com>
2025-08-18 10:54:27 -0300

    handleRemoveButton

--- form-zod/src/components/PostList.tsx
+++ form-zod/src/components/PostList.tsx
@@ -5,6 +5,10 @@ export const PostList = () => {
 
     const postCtx = useContext (PostCtx);
 
+    const handleRemoveButton = (id: number) =>{
+      postCtx?.removePost(id);
+    }
+
 
     
     return (
@@ -13,7 +17,7 @@ export const PostList = () => {
     <div key={item.id} className="p-3 border-b border-gray-500">
     <div className="text-xl font-bold mb-2">{item.title}</div>
     <div className="text-sm">{item.body}</div>  
-    <button onClick={() => postCtx.removePost(item.id)}> [remover] </button>
+    <button onClick={()=> handleRemoveButton(item.id)}> [remover] </button>
     </div>
     ))}
   </div>
