fcb71351ef930e2ba768ad8aff27d74f3d152903
joaodlouro <joaovitorvx1315@gmail.com>
2025-08-29 11:38:46 -0300

    chat test

--- form-zod/src/components/NameInput.tsx
+++ form-zod/src/components/NameInput.tsx
@@ -1,8 +1,25 @@
 import { useState } from "react";
+import { useUser } from "@/contexts/UsersCtx";
+import { KeyboardEvent } from "react";
+
 
 
 export const NameInput = () => {
-    const [NameInput, setName] = useState('')
+    const UserCtx = useUser();
+    const [NameInput, setName] = useState('');
+
+   const handleKeyUpAction = (event: React.KeyboardEvent<HTMLInputElement>) => {
+  if (event.key === 'Enter') {
+    const normalized = NameInput.trim().toLowerCase();
+    
+    if (normalized !== '' && normalized !== 'bot') {
+      UserCtx?.setUser(NameInput.trim()); 
+    }
+  }
+}
+
+
+
   return (
     <div className="mt-14">
         <p>Qual seu nome?</p>
@@ -10,9 +27,10 @@ export const NameInput = () => {
             <input type="text" className="flex-1 border border-white/30 rounded-md px-4 py-3 text-white bg-white/10 outline-none"
             value={NameInput}
             onChange={e => setName(e.target.value)}
+            onKeyUp={handleKeyUpAction}
             />
             
     </div>
     </div>
   );
-}
+} 
