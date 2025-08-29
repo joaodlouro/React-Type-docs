a470bbb9956f293ff3684179894489d421411ece
joaodlouro <joaovitorvx1315@gmail.com>
2025-08-29 16:00:49 -0300

    fix

--- form-zod/src/app/page.tsx
+++ form-zod/src/app/page.tsx
@@ -1,25 +1,12 @@
-"use client"
+"use client";
 
-import { ChatProvider } from "@/contexts/ChatCtx";
-import { UserProvider } from "@/contexts/UsersCtx";
-import { Chat } from "@/components/Chat";
+const Page = () => {
+return (
 
-
-const Page= () => {
-
-
-
-  return(
-    <div className="container mx-auto mx-auto max-w-lg px2">
-      <UserProvider>
-        <ChatProvider>
-          <h1 className="text-3xl font-bold my-3 text-center">Chat aqui</h1>
-          <Chat />
-        </ChatProvider>
-
-      </UserProvider>
-    </div>
-  )
+  <div className="container flex-center mx-auto">
+    <p>salve</p>
+  </div>
+)
 }
 
 export default Page;
