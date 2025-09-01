75cb6cac05274334d5f2076a6c1d048e1d60d4f9
joaodlouro <joaovitorvx1315@gmail.com>
2025-09-01 16:16:56 -0300

    Fazendo um Fetch POST

--- form-zod/docs/pagesDocs/Usando Async/Await.tsx
+++ form-zod/docs/pagesDocs/Usando Async/Await.tsx
@@ -2,6 +2,65 @@
 
 import { useEffect, useState } from "react";
 import { User } from "../types/User";
+
+
+
+const Page = () => {
+
+  const [users, setUsers] = useState<User[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const geyUsers = async () => {
+    setLoading(true);
+    try {
+  //fetch busca os dados
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const json = await res.json(); //transformar em json 
+  setUsers(json); 
+    } catch (erro) {
+      console.log("Deu erro");
+    }
+    setLoading(false);
+
+  };
+ 
+  useEffect(() => {
+    geyUsers();
+    }, []);  //dessa forma fica bem reduzido
+     
+
+return (
+
+  <div className="container flex-center mx-auto">
+    <h1 className="text-3xl">Lista</h1>
+
+    {loading && "carregando..."}
+    {!loading  && users.length > 0 &&
+
+    <ul> 
+    {users.map(item => (
+      <li key={item.id}>{item.name}
+      ({item.address.city})</li>))
+    }
+    </ul>
+}
+{!loading && users.length === 0 && <p>Não há usuários!</p>}
+
+  </div>
+)
+}
+
+export default Page;
+
+
+
+
+
+
+"use client";
+
+import { useEffect, useState } from "react";
+import { User } from "../types/User";
 import { title } from "process";
 
 
@@ -33,7 +92,7 @@ const Page = () => {
     const handleAddNewPost = async () => {
       const res = await fetch("https://jsonplaceholder.typicode.com/posts"
       , {
-        method: "POST",                //para outras requisições só mudar o method
+        method: "POST",
         headers: {
           "Content-Type" : "application/json; charset=UTF-8"
         },
