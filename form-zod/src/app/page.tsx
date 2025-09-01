efa6be16445781fac2b2d76b6dbcd885059863d8
joaodlouro <joaovitorvx1315@gmail.com>
2025-08-31 22:27:18 -0300

    carregando users.length .then

--- form-zod/src/app/page.tsx
+++ form-zod/src/app/page.tsx
@@ -1,16 +1,20 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { User } from "../types/User";
 
 
 
 const Page = () => {
+
+  const [users, setUsers] = useState<User[]>([]);
  
   useEffect(() => {
      fetch ("https://jsonplaceholder.typicode.com/posts")
       .then(res => res.json())  
       .then(json => {
-        console.log(json[0]);
+        setUsers(json);
+   
       }) 
 
   }, []);
@@ -19,6 +23,18 @@ return (
 
   <div className="container flex-center mx-auto">
     <h1 className="text-3xl">Lista</h1>
+
+    {users.length <= 0 && "carregando..."}
+    {users.length > 0 &&
+
+    <ul> 
+    {users.map(item => (
+      <li key={item.id}>{item.name}
+      ({item.address.city})</li>))
+    }
+    </ul>
+}
+
   </div>
 )
 }
